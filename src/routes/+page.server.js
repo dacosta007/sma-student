541c469d0382646062f48199410c2a79cd1517fe
Olakunle Dacosta <dacosta00736@gmail.com>
2023-11-20 15:42:57 +0100

    update for new sch session

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,6 @@
 import { branches } from "$db/collections/branches"
 
 export async function load() {
-  // const baseUrl = 'https://getpantry.cloud/apiv1/pantry/8e96f3a9-a647-4f37-931d-586203d634b3/basket'
   try {
     let query = { "branch.code": "002" }
     let opt = { projection: { _id: 0 } }
